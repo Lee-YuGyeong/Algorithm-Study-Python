14664dc1f4e3feee33afb093e76303884a590135
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-11-21 01:59:25 +0900

    피보나치 수 5

--- Baekjoon/10872.py
+++ Baekjoon/10872.py
@@ -1,6 +1,8 @@
-def f(n):
-    if n<=1:
-        return 1
-    return n*f(n-1)
 n = int(input())
-print(f(n))
+
+def cal(n):
+    if n<=1:
+        return n
+    return cal(n-1)+cal(n-2)
+
+print(cal(n))
