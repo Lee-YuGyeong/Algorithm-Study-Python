bedbef3f8ba74ad2cb6427f4bb4f401bf679297b
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-11-28 18:31:26 +0900

    좌표 압축

--- Baekjoon/18870.py
+++ Baekjoon/18870.py
@@ -2,6 +2,7 @@ import sys
 input =sys.stdin.readline
 n = int(input())
 arr = list(map(int,input().split()))
-s = set(arr)
-
-print(arr)
+s = list(sorted(set(arr)))
+dic={value:index for index,value in enumerate(s)}
+for i in arr:
+    print(dic[i],end=" ")
