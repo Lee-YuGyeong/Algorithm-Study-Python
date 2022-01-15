53fc1cf923c473b543f50ddb3aab788aeb43ebb5
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-01-16 00:39:54 +0900

    회의실 배정

--- LeeYuGyeong/211224~/1931.py
+++ LeeYuGyeong/211224~/1931.py
@@ -1,9 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-
-d = [0]*(100001)
-arr=[]
-for i in range(n):
-    arr.append(list(map(int,input().split())))
-print(arr)
-
-for i in range(n):
+t = []
+for _ in range(n):
+    a,b = map(int,input().split())
+    t.append([a,b])
+end,cnt=0,0
+for a,b in sorted(t,key=lambda x:(x[1],x[0])):
+    if end <= a:
+        cnt+=1
+        end = b
+print(cnt)
