828d7f9caacea2899862d63c0d56d2e60e437d63
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-05-15 00:06:55 +0900

    팰린드롬 만들기

--- 2022년 상반기/1213.py
+++ 2022년 상반기/1213.py
@@ -1,24 +1,24 @@
 from collections import Counter
 ss = list(input())
 
-dic = Counter(ss)
-cnt=0
+dic = Counter(ss) # 개수 세주기
+cnt=1
 answer=[]
 one = ''
 for d in dic:
-    for i in range(dic[d]//2):
+    for i in range(dic[d]//2): # 반만큼 answer에 더하기
         answer.append(d)
-    if dic[d]%2!=0:
-        if cnt==1:
-            print("I'm Sorry Hansoo")
+    if dic[d]%2!=0: # 홀수일경우
+        if cnt==2: # 홀수인게 한개가 넘을경우
+            print("I'm Sorry Hansoo") # 실패
             break
         else:
-            cnt+=1
-            one=d
+            cnt+=1 # 홀수가 하나일경우
+            one=d # 홀수 값 저장
 
 else:
-    answer.sort()
-    print('%s%s%s'%(''.join(answer),one,''.join(answer[::-1])))
+    answer.sort() # 사전순으로 정렬해주기
+    print('%s%s%s'%(''.join(answer),one,''.join(answer[::-1]))) # 정렬된값 + 홀수 값 + 정렬 역순 값
     
 
     
