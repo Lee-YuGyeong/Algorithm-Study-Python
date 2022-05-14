19e8d7296c6dc1434dd3b4543e65408d9930d24d
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-05-14 13:55:47 +0900

    Update 9996.py
    한국이 그리울 땐 서버에 접속하지

--- 2022년 상반기/9996.py
+++ 2022년 상반기/9996.py
@@ -1,19 +1,19 @@
 n = int(input())
-ss = list(input().split('*'))
+ss = list(input().split('*')) # *을 기준으로 나눠줌
 for i in range(n):
     aa = input()
-    if len(ss[0])+len(ss[1])<=len(aa):
-        for a,b in zip(ss[0],aa):
-            if a!=b: 
+    if len(ss[0])+len(ss[1])<=len(aa): # 입력받은 문자열의 길이가 같거나 더 길어야함
+        for a,b in zip(ss[0],aa): # * 앞에 있는 문자를 비교 
+            if a!=b: # 다르면 NE 출력
                 print('NE')
                 break
-        else:
-            for a,b in zip(ss[1][::-1],aa[::-1]):
-                if a!=b: 
-                    print('NE')
+        else: # 다른게 없었다면 
+            for a,b in zip(ss[1][::-1],aa[::-1]): # * 뒤의 문자를 비교. 이때 문자를 뒤집어서 비교
+                if a!=b: # 다르면 
+                    print('NE') # NE 출력
                     break
             else:
-                print('DA')
-    else:
+                print('DA') # 다른게 없었다면 DA 출력
+    else: # 입력받은 문자열의 길이가 더 짧으면 NE 출력
         print('NE')
     
