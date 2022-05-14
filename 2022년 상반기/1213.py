b58a64ea81834a196b48ae0bfe777c691e37d8a1
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-05-15 00:03:32 +0900

    팰린드롬 만들기

--- 2022년 상반기/1213.py
+++ 2022년 상반기/1213.py
@@ -1,6 +1,27 @@
+from collections import Counter
 ss = list(input())
 
-dic = []
+dic = Counter(ss)
+cnt=0
+answer=[]
+one = ''
+for d in dic:
+    for i in range(dic[d]//2):
+        answer.append(d)
+    if dic[d]%2!=0:
+        if cnt==1:
+            print("I'm Sorry Hansoo")
+            break
+        else:
+            cnt+=1
+            one=d
 
-for s in ss:
+else:
+    answer.sort()
+    print('%s%s%s'%(''.join(answer),one,''.join(answer[::-1])))
+    
+
+    
+    
+
     
