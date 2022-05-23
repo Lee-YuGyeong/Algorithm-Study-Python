b8fee217b7851423b0c6edc712ff26b16fd9c82f
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2022-05-23 16:50:37 +0900

    문자열 폭발

--- 2022년 상반기/9935.py
+++ 2022년 상반기/9935.py
@@ -4,9 +4,9 @@ m = list(input())
 stack = []
 
 for i in range(len(ss)):
-    stack.append(ss[i])
-    if stack[-len(m):] == m:
-        del stack[-len(m):]
+    stack.append(ss[i]) # 스택에 하나씩 추가
+    if stack[-len(m):] == m: # 스택의 마지막이 m 문자열과 같으면
+        del stack[-len(m):] # 삭제
 
 if stack: print("".join(stack))
 else: print("FRULA")
